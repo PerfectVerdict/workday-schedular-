f224eaf9d3dff7e0284a34477efc229895b7b56d
76500833 <145300587+76500833@users.noreply.github.com>
2023-11-29 19:20:02 -0500

    Got classses to add
    had to convert to 14 hour format in html

--- script.js
+++ script.js
@@ -2,7 +2,30 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  //! Current Day and Time:
+  // TODO: Add code to apply the past, present, or future class to each time
+  // TODO block by comparing the id to the current hour.
+  //*Iterates over each jquery object (each element with a class
+  //* of 'time-block')
+  $('.time-block').each(function() {
+    //*declaring the div that we can traverse from on the next line
+    //* to get the time to compare to 'currentHour'
+    let timeIdDiv = $(this); //*'this' refers to the current element
+                             //*that the .each function operates on
+    let time = timeIdDiv.attr('id'); //*isolating the id
+    let timeInt = Number(time.split('-')[1]); //* isolating the integer
+    //! Credit to github Ai sidebar                                          
+    let currentHour = Number(dayjs().format('H'))
+    console.log(currentHour)
+
+
+    if (timeInt === currentHour)
+      timeIdDiv.addClass("present")
+    else if (timeInt < currentHour)
+      timeIdDiv.addClass("past")
+    else if (timeInt > currentHour)
+      timeIdDiv.addClass("future")
+  });
+  // ! Current Day and Time:
   let currentDay = dayjs().format('dddd, MMMM D YYYY')
   let currentTime = dayjs().format('h:mm:ss a')
   //! Append Current Day and Time:
@@ -26,20 +49,28 @@ $(function () {
       // *useful when saving the description in local storage?
     let timeIdDiv = $(this).parent()
     let time= $(timeIdDiv).attr('id')
+    //! Credit to Github ai sidepanel///////////////////
+      //*Get the integer of the div so we can later compare it and add
+      //* or remove classes. .split('-') turns the id into an array of
+      //* strings, with "hour" at idex 0 and "(the hour itself)" at
+      //* index one. Thats why we use [1].
+    let timeInt = Number(time.split('-')[1]); 
     let userInput = $(timeIdDiv).find('textarea').val();
+    //* add user input to object
     obj[time] = userInput;
-    console.log(obj);
+    //! /////////////
+    
+    //TODO HINTS: How can the id
+    // TODO attribute of each time-block be used to conditionally add or remove the
+
+
+    // past, present, and future classes? How can Day.js be used to get the
+    // current hour in 24-hour time?
+    //
+    // TODO: Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements. HINT: How can the id
+    // attribute of each time-block be used to do this?
+    //
+    // TODO: Add code to display the current date in the header of the page.
+    })
   })
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
